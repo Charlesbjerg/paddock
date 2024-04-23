0619d28069041a51dfd3974ad5d0828d8ec8b5dc
Charlie Underhill <c.underhill@wethrive.agency>
2024-04-23 14:26:21 +0100

    Setup basic race pages

--- app/races/[ref]/page.tsx
+++ app/races/[ref]/page.tsx
@@ -1,20 +1,38 @@
 import { db } from "@/db/db";
 import { drivers, races } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
-export default async function Page({ params }: { params: { ref: string } }) {
-  const [year, round] = params.ref.split("-");
+export default async function Page({ params }: { params: { ref: number } }) {
+  // const [year, round] = params.ref.split("-");
   console.log(params);
-  const race = await db.query.races.findMany({
-    where: {
-      year: parseInt(year),
-      round: parseInt(round),
-    },
+  const race = await db.query.races.findFirst({
+    where: eq(races.id, params.ref),
+    with: {
+      circuit: true,
+      results: {
+        with: {
+          driver: true,
+          constructor: true,
+        }
+      },
+    }
   });
 
   return (
-    <div>
-      <p>Race!</p>
-      <pre>{JSON.stringify(race)}</pre>
+    <div className="container py-12">
+      <h1 className="text-4xl">Race!</h1>
+      <p>Results of the {race.year} {race.name}</p>
+      <div className="grid gap-y-4">
+        {race?.results.map(result => (
+          <article key={result.id} className="p-4 border border-zinc-400 rounded">
+            <div className="inline-block px-3 py-1 bg-zinc-200 font-sans rounded mb-4">{result.position ? `P${result.position}` : result.positionText}</div>
+            <h2 className="text-2xl">{result.driver?.forename} {result.driver?.surname}</h2>
+            <p>Time: {result.time}</p>
+            <p>Driver's fastest lap: {result.fastestLapTime}</p>
+          </article>
+        ))}
+      </div>
+      <pre>{JSON.stringify(race, null, 4)}</pre>
     </div>
   );
 }
---
+++ app/races/page.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { db } from "@/db/db";
+import Link from "next/link";
+
+export default async function Page() {
+
+    const races = await db.query.races.findMany({
+        with: {
+            circuit: true
+        }
+    });
+
+  return (
+    <div className="container py-12">
+        <h1>Races</h1>
+        <div className="grid grid-cols-3 gap-6">
+        {races.map(race => (
+            <article key={race.id} className="p-4 rounded shadow">
+                <h2>{race.year} {race.name}</h2>
+                <p>{race.circuit?.location}: {race.circuit?.name}</p>
+                <Link href={`/races/${race.year}-${race.round}`}>
+                    <Button>View</Button>
+                </Link>
+            </article>
+        ))}
+        </div>
+    </div>
+  )
+}
